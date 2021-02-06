794e43066947dbebaa866129609121ff15b48907
Luis E Lozano <lozano_22_lalo@yahoo.com>
2021-02-06 18:09:17 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,5 +1,5 @@
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  # status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase = 'It's-a me, Mario!'
 end
 
