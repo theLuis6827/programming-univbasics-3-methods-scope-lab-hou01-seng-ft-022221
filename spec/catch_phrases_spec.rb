2ada1a92f8a7ae26b4084fc4085eb30b97d9db58
Luis E Lozano <lozano_22_lalo@yahoo.com>
2021-02-06 18:22:58 +0000

    Automatically backed up by Learn

--- spec/catch_phrases_spec.rb
+++ spec/catch_phrases_spec.rb
@@ -16,7 +16,7 @@ end
 
 describe "link" do
   it "puts out 'It's Dangerous To Go Alone! Take This.'" do
-    says = "It's Dangerous To Go ALone! Take This."
+    says = "It's Dangerous To Go Alone! Take This."
     expect{link}.to output("It's Dangerous To Go Alone! Take This.\n").to_stdout
   end
 end
