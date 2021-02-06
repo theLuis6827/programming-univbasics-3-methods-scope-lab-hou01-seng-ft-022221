940d8d1ced29d05ad59edf744ebc49fdf054913b
Luis E Lozano <lozano_22_lalo@yahoo.com>
2021-02-06 18:13:46 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,5 +1,5 @@
 def mario (phrase = 'It's-a me, Mario!'')
-  puts phrase = 'It's-a me, Mario!'
+  puts phrase
 end
 
 def toadstool (status= 'Thank You Mario! But Our Princess Is In Another Castle')
