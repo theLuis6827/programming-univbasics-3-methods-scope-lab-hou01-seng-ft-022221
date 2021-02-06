8cf37b68a10748a963b83812e77d05fa66487cca
Luis E Lozano <lozano_22_lalo@yahoo.com>
2021-02-06 18:25:18 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -6,6 +6,6 @@ def toadstool (status= 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
 
-def link (says= "It's Dangerous To Go ALone! Take This.")
+def link (says= "It's Dangerous To Go Alone! Take This.")
   puts says
 end
